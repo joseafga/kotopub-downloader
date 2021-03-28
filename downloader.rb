3bb2a38aeb16a228c04b50c283194761f128e820
José Almeida <jose.afga@gmail.com>
2021-03-28 02:20:40 -0300

    Make downloader more flexible with configuration and multiples ebooks

--- downloader.rb
+++ downloader.rb
@@ -3,67 +3,81 @@
 
 require 'fileutils'
 require 'open-uri'
+require 'yaml'
 
-BOOK = {
-  id: 'xyz',
-  name: 'Title'
-}.freeze
-URL = "https://example.com/books/#{BOOK[:id]}/EPUB"
+CONFIG = YAML.load_file('config.yml')
+Ebook = Struct.new(:id, :name)
 
-def download2file(target)
-  path = "#{__dir__}/#{BOOK[:name]}/#{target}"
-  url = "#{URL}/#{target}"
+# Download content of a ebook
+class Downloader
+  def initialize(ebook)
+    @ebook = ebook
+    @download_dir = "#{CONFIG[:download]}/#{@ebook.name}"
+    @url = "https://example.com/books/#{@ebook.id}/EPUB"
+  end
 
-  # create full path directory
-  directory = File.dirname(path)
-  FileUtils.mkdir_p directory unless Dir.exist?(directory)
+  def start
+    puts "Downloading #{@ebook.name} metadata ..."
+    download_meta
+    links = parse_package
 
-  # download
-  URI.parse(url).open do |content|
-    File.open(path, 'wb') { |f| f.write(content.read) }
+    links.each do |link|
+      next if link.start_with? '..' # not member of epub
+
+      puts "Downloading #{link} ..."
+      download2file("EPUB/#{link}")
+    end
+
+    puts "Done #{@ebook.name}"
+  end
+
+  private
+
+  def download2file(target)
+    path = "#{@download_dir}/#{target}"
+    url = "#{@url}/#{target}"
+
+    # create full path directory
+    directory = File.dirname(path)
+    FileUtils.mkdir_p directory unless Dir.exist?(directory)
+
+    # download
+    URI.parse(url).open do |content|
+      File.open(path, 'wb') { |f| f.write(content.read) }
+    end
+  end
+
+  def download_meta
+    download2file('mimetype')
+    download2file('META-INF/container.xml')
+    download2file('EPUB/package.opf')
+    # stylesheet
+    download2file('EPUB/css/base.css')
+    download2file('EPUB/css/global.css')
+    # download2file('EPUB/js/global.js') # really need this?
+  end
+
+  def parse_package
+    links = File.readlines("#{@download_dir}/EPUB/package.opf").grep(/href="/)
+    stylesheets = []
+
+    # clean urls
+    links.map! do |link|
+      link[/href="([^"]*)"/, 1]
+    end
+
+    links.each do |link|
+      css = link[%r{xhtml/epub[^/]*}]
+      stylesheets << "#{css}/OEBPS/css/idGeneratedStyles.css" if css
+    end
+
+    links + stylesheets.uniq
   end
 end
 
-def download_meta
-  download2file('mimetype')
-  download2file('META-INF/container.xml')
-  download2file('EPUB/package.opf')
-  # stylesheet
-  download2file('EPUB/css/base.css')
-  download2file('EPUB/css/global.css')
-  download2file('EPUB/js/global.js')
+CONFIG[:ebooks].each do |eb|
+  ebook = Ebook.new(eb[:id], eb[:name])
+
+  downloader = Downloader.new ebook
+  downloader.start
 end
-
-def parse_package
-  links = File.readlines("#{__dir__}/#{BOOK[:name]}/EPUB/package.opf").grep(/href="/)
-  stylesheets = []
-
-  # clean urls
-  links.map! do |link|
-    link[/href="([^"]*)"/, 1]
-  end
-
-  links.each do |link|
-    css = link[%r{xhtml/epub[^/]*}]
-    stylesheets << "#{css}/OEBPS/css/idGeneratedStyles.css" if css
-  end
-
-  links + stylesheets.uniq
-end
-
-def main
-  puts 'Downloading metadata ...'
-  download_meta
-  links = parse_package
-
-  links.each do |link|
-    next if link.start_with? '..' # not member of epub
-
-    puts "Downloading #{link} ..."
-    download2file("EPUB/#{link}")
-  end
-
-  puts 'Done'
-end
-
-main
